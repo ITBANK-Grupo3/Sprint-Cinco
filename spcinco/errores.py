0ba0a623080c5e4664f402a13acf3c4adf5b45b7
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-07-17 07:21:16 -0300

    clases razones con filtrado según tipo de cliente + retorno de eventos procesados en forma de diccionario

--- spcinco/errores.py
+++ spcinco/errores.py
@@ -1,23 +1,29 @@
+from re import I
 import clases.razones as rz
 
 class FiltradoDeErrores(object):
   def __init__(self, eventos, tipoCliente):
     self.errores = [error for error in eventos if error["estado"] == "RECHAZADA"]
-    for error in self.errores:
-      if error["tipo"] == "ALTA_CHEQUERA":
-        print(razon := rz.RazonAltaChequera(error, tipoCliente))
-      
-      elif error["tipo"] == "ALTA_TARJETA_CREDITO":
-        print(razon := rz.RazonAltaTarjetaDeCredito(error, tipoCliente))
+    self.eventosProcesados = []
+    for evento in eventos:
+      if evento["estado"] == "RECHAZADA":
+        if evento["tipo"] == "ALTA_CHEQUERA":
+          razon = str(rz.RazonAltaChequera(evento, tipoCliente))
 
-      elif error["tipo"] == "COMPRA_DOLAR":
-        print(razon := rz.RazonCompraDolar(error, tipoCliente))
+        elif evento["tipo"] == "ALTA_TARJETA_CREDITO":
+          razon = str(rz.RazonAltaTarjetaDeCredito(evento, tipoCliente))
 
-      elif error["tipo"] == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO":
-        print(razon := rz.RazonRetiroEfectivo(error, tipoCliente))
+        elif evento["tipo"] == "COMPRA_DOLAR":
+          razon = str(rz.RazonCompraDolar(evento, tipoCliente))
 
-      elif error["tipo"] == "TRANSFERENCIA_ENVIADA":
-        print(razon := rz.RazonTransferenciaEnviada(error, tipoCliente))
+        elif evento["tipo"] == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO":
+          razon = str(rz.RazonRetiroEfectivo(evento, tipoCliente))
 
-      elif error["tipo"] == "TRANSFERENCIA_RECIBIDA":
-        print(razon := rz.RazonTransferenciaRecibida(error, tipoCliente))
+        elif evento["tipo"] == "TRANSFERENCIA_ENVIADA":
+          razon = str(rz.RazonTransferenciaEnviada(evento, tipoCliente))
+
+        elif evento["tipo"] == "TRANSFERENCIA_RECIBIDA":
+          razon = str(rz.RazonTransferenciaRecibida(evento, tipoCliente))
+      else:
+        razon = "Solicitud realizada con éxito"
+      self.eventosProcesados.append({"cuentaNumero": evento["cuentaNumero"],"SolicitudNro":evento["numero"], "estado":evento["estado"],"razon": razon, "fecha": evento["fecha"]})
