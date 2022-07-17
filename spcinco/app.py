9e34c595e521ca38f3c5e93690cf3a6eade9b4d1
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-07-17 07:36:55 -0300

    app actualizada con razones y errores

--- spcinco/app.py
+++ spcinco/app.py
@@ -1,3 +1,10 @@
 import lector
+import errores
 
-data = lector.Lector('eventos-classic.json')
+data = lector.Lector('../eventos-classic.json')
+
+eventos = data.eventos
+cliente = data.cliente
+erroresData = errores.FiltradoDeErrores(eventos, cliente.tipo)
+
+eventosProcesados = erroresData.eventosProcesados
