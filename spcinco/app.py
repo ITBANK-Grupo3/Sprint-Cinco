56244c4c53eb480ac8beb5598c4d96eacbc0544c
agustinvillagran <87080985+aguvilla@users.noreply.github.com>
2022-07-17 22:22:21 -0300

    Update app.py

--- spcinco/app.py
+++ spcinco/app.py
@@ -14,6 +14,7 @@ clienteClassic = cuentaClassic.cuenta.tipo_de_cliente
 erroresDataClassic = errores.FiltradoDeErrores(eventosClassic, clienteClassic.tipo)
 
 eventosProcesadosClassic = erroresDataClassic.eventosProcesados
+salidaWeb.SalidaHTML.procesar(clienteClassic,eventosProcesadosClassic, "Classic")
 
 # Procesamiento Cliente Gold
 cuentaGold = lector.Lector('../eventos-gold.json')
@@ -23,6 +24,7 @@ clienteGold = cuentaGold.cuenta.tipo_de_cliente
 erroresDataGold = errores.FiltradoDeErrores(eventosGold, clienteGold.tipo)
 
 eventosProcesadosGold = erroresDataGold.eventosProcesados
+salidaWeb.SalidaHTML.procesar(clienteGold, eventosProcesadosGold, "Gold")
 
 #Procesamiento Cliente Black
 cuentaBlack = lector.Lector('../eventos-black.json')
@@ -32,3 +34,4 @@ clienteBlack = cuentaBlack.cuenta.tipo_de_cliente
 erroresDataBlack = errores.FiltradoDeErrores(eventosBlack, clienteBlack.tipo)
 
 eventosProcesadosBlack = erroresDataBlack.eventosProcesados
+salidaWeb.SalidaHTML.procesar(clienteBlack, eventosProcesadosBlack, "Black")
